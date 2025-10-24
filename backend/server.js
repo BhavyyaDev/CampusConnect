9b4436c2659006c922cb35f93e14156f9a45ffb6
Bhavya <bhavya@Mac.lan>
2025-10-24 09:06:28 +0530

    feat: Setup core backend server and DB connection

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use((req, res, next) => {
+  // THIS IS THE TEST:
+  console.log(`--- REQUEST RECEIVED: ${req.method} ${req.path} ---`); 
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  
+  next();
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
